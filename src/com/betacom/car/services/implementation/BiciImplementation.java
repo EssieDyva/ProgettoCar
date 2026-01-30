b7abd70945aa99fca1ece899617d8b97025d98a5
EssieDyva <102621809+EssieDyva@users.noreply.github.com>
2026-01-30 15:10:32 +0100

    implementation added

--- src/com/betacom/car/services/implementation/BiciImplementation.java
+++ src/com/betacom/car/services/implementation/BiciImplementation.java
@@ -11,17 +11,25 @@ public class BiciImplementation extends AbstractVeicoloImp{
 	@Override
 	protected Veicoli creaVeicolo(Integer id, String tipoVeicolo, Integer numeroRuote, String tipoAlimentazione, String categoria, String colore, String marca, Integer annoProduzione, String modello, Map<String, String> mappa) {
 
-		System.out.println("inizio crea veicolo macchina");
+		System.out.println("inizio crea veicolo bici");
 
-		Integer numeroMarce=Integer.parseInt(mappa.get("numeroMarce"));
-		String tipoFreno = mappa.get("tipoFreno");
-		String tipoSospensione = mappa.get("tipoSospensione");
-		Boolean isPieghevole = Boolean.parseBoolean("isPieghevole");
+		try {
+			Integer numeroMarce=Integer.parseInt(mappa.get("numeroMarce"));
+			String tipoFreno = mappa.get("tipoFreno");
+			String tipoSospensione = mappa.get("tipoSospensione");
+			Boolean isPieghevole = Boolean.parseBoolean("isPieghevole");
 
-		if(numeroRuote.intValue() == 2)
+			if(numeroRuote.intValue()  <2 || numeroRuote.intValue()>3) {
+				System.out.println("Errore: Numero ruote non valido");
+				return null;
+			}
+
 			return new Bici(id, tipoVeicolo, numeroRuote, tipoAlimentazione, categoria, colore, marca, annoProduzione, modello, numeroMarce, tipoFreno, tipoSospensione, isPieghevole);
 
-		return null;
 
+		} catch (Exception e) {
+			System.out.println("Errore nei dati specifici della Bici: " + e.getMessage());
+			return null;
+		}
 	}
 }
